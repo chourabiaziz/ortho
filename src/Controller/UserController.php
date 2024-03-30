fe50fd1b16b56e5b590da82b1a9d516953311fe8
chourabi aziz <chourabiaziz007@gmail.com>
2024-03-30 05:19:28 +0100

    logo

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -43,7 +43,7 @@ class UserController extends AbstractController
             $entityManager->persist($user);
             $entityManager->flush();
 
-            return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('app_user_index');
         }
 
         return $this->render('user/new.html.twig', [
